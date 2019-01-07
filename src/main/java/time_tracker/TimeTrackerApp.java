69a0f08ede97f62fc455bc89d4a020da84fabbbe
Andrei <mairovichaa@yandex.ru>
2019-01-07 16:32:43 +0300

    Create two cols - for start and end

--- src/main/java/time_tracker/TimeTrackerApp.java
+++ src/main/java/time_tracker/TimeTrackerApp.java
@@ -2,16 +2,24 @@ package time_tracker;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TimeTrackerApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        VBox root = new VBox();
+        Text startTitle = new Text("Start");
+        VBox startVbox = new VBox(startTitle);
 
+        Text endTitle = new Text("End");
+        VBox endVbox = new VBox(endTitle);
 
+        HBox entriesTable = new HBox(startVbox, endVbox);
+
+        VBox root = new VBox(entriesTable);
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
