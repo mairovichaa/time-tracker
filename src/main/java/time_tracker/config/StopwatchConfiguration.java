915e94d0bc2bae68ce9285488f252da412647510
mairovichaa <mairovichaa@yandex.ru>
2022-07-10 13:49:26 +0300

    Get rid of StopwatchRecordConsoleRepository

--- src/main/java/time_tracker/config/StopwatchConfiguration.java
+++ src/main/java/time_tracker/config/StopwatchConfiguration.java
@@ -4,7 +4,6 @@ import time_tracker.annotation.NonNull;
 import time_tracker.component.stopwatch.StopWatchTab;
 import time_tracker.config.properties.StopwatchProperties;
 import time_tracker.model.StopWatchAppState;
-import time_tracker.repository.StopwatchRecordConsoleRepository;
 import time_tracker.repository.StopwatchRecordFileRepository;
 import time_tracker.repository.StopwatchRecordRepository;
 import time_tracker.service.DefaultStopwatchRecordService;
@@ -15,12 +14,6 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 
 public class StopwatchConfiguration {
-
-    //    TODO move to enum???
-    private static final String CONSOLE_REPOSITORY_TYPE = "console";
-    private static final String FILE_REPOSITORY_TYPE = "file";
-    // TODO move to configuration file
-    private final String typeOfRepository = FILE_REPOSITORY_TYPE;
 
     public StopwatchRecordService stopwatchRecordService(
             @NonNull final StopWatchAppState stopWatchAppState,
@@ -41,15 +34,9 @@ public class StopwatchConfiguration {
     public StopwatchRecordRepository stopwatchRecordRepository(
             @NonNull final StopwatchProperties stopwatchProperties
     ) {
-        if (typeOfRepository.equals(FILE_REPOSITORY_TYPE)) {
-            var folderWithData = stopwatchProperties.getFolderWithData();
-            var path = Paths.get(folderWithData);
-            return new StopwatchRecordFileRepository(path);
-        }
-        if (typeOfRepository.equals(CONSOLE_REPOSITORY_TYPE)) {
-            return new StopwatchRecordConsoleRepository();
-        }
-        throw new IllegalArgumentException("Unknown type of repository: " + typeOfRepository);
+        var folderWithData = stopwatchProperties.getFolderWithData();
+        var path = Paths.get(folderWithData);
+        return new StopwatchRecordFileRepository(path);
     }
 
     public StopWatchTab stopWatchTab(
