a6eca15dc8ebe8045487365c827cdc5e60eff467
andrei.mairovich <mairovichaa@yandex.ru>
2024-01-13 22:39:54 +0400

    Add tracked filter to daily statistics

--- desktop/src/main/java/time_tracker/component/statistics/DailyStatisticsVBox.java
+++ desktop/src/main/java/time_tracker/component/statistics/DailyStatisticsVBox.java
@@ -1,5 +1,6 @@
 package time_tracker.component.statistics;
 
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
@@ -8,36 +9,33 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import time_tracker.Utils;
-import time_tracker.component.statistics.model.DayStatistics;
 import time_tracker.common.GlobalContext;
-import time_tracker.config.properties.StopwatchProperties;
+import time_tracker.component.statistics.model.DayStatistics;
+import time_tracker.model.DayData;
 import time_tracker.model.StopWatchAppState;
-import time_tracker.service.TimeService;
 
-import java.time.LocalDate;
-import java.util.stream.IntStream;
+import java.util.function.Predicate;
 
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
+import static javafx.collections.FXCollections.observableArrayList;
 import static time_tracker.Utils.DATE_FORMAT_WITH_SHORT_DAY_NAME;
 import static time_tracker.component.Utils.load;
 
 public class DailyStatisticsVBox extends VBox {
 
     @FXML
+    private MFXComboBox<String> trackedComboBox;
+    @FXML
     private MFXTableView<DayStatistics> table;
-
-    private final TimeService timeService;
-    private final StopwatchProperties stopwatchProperties;
     private final StopWatchAppState stopWatchAppState;
 
     public DailyStatisticsVBox() {
         load("/fxml/statistics/DailyStatisticsVBox.fxml", this);
-
         stopWatchAppState = GlobalContext.get(StopWatchAppState.class);
-        timeService = GlobalContext.get(TimeService.class);
-        stopwatchProperties = GlobalContext.get(StopwatchProperties.class);
 
+        trackedComboBox.selectIndex(0);
 
         MFXTableColumn<DayStatistics> dateColumn = new MFXTableColumn<>("Date");
         dateColumn.setRowCellFactory(stats -> new MFXTableRowCell<>(DayStatistics::getDate));
@@ -65,44 +63,46 @@ public class DailyStatisticsVBox extends VBox {
 
     @FXML
     private void refresh() {
-        var stopwatchDatesProperties = stopwatchProperties.getDates();
-
-        var today = timeService.today();
-        var amountOfDaysToShow = stopwatchDatesProperties.getAmountOfDaysToShow();
-        var dates = getDayStatistics(today, amountOfDaysToShow);
-        table.setItems(FXCollections.observableArrayList());
+        var dates = getDayStatistics();
+        table.setItems(observableArrayList());
         table.setItems(dates);
     }
 
-    private ObservableList<DayStatistics> getDayStatistics(LocalDate today, int amountOfDaysToShow) {
-        return IntStream.range(0, amountOfDaysToShow)
-                .mapToObj(today::minusDays)
-                .map(it -> {
+    private ObservableList<DayStatistics> getDayStatistics() {
+        String trackedSelected = trackedComboBox.getSelectedItem();
+        Predicate<DayData> dayDataPredicate;
+        if (trackedSelected.equals("all")) {
+            dayDataPredicate = it -> true;
+        } else if (trackedSelected.equals("yes")) {
+            dayDataPredicate = DayData::isTracked;
+        } else {
+            dayDataPredicate = Predicate.not(DayData::isTracked);
+        }
+
+        return stopWatchAppState.getDateToDayData()
+                .values()
+                .stream()
+                .filter(dayDataPredicate)
+                .sorted(comparing(DayData::getDate).reversed())
+                .map(dayData -> {
                     var builder = DayStatistics.builder();
-                    var date = DATE_FORMAT_WITH_SHORT_DAY_NAME.format(it);
+                    builder.amount(dayData.getAmountOfRecords());
+                    var totalSecs = dayData.getTotalInSecs();
+                    var duration = Utils.formatDuration(totalSecs);
+                    builder.total(duration);
 
-                    var dayData = stopWatchAppState.getDateToDayData().get(it);
-                    if (dayData != null) {
-                        builder.amount(dayData.getAmountOfRecords());
-                        var totalSecs = dayData.getTotalInSecs();
-                        var duration = Utils.formatDuration(totalSecs);
-                        builder.total(duration);
+                    var timeToWorkLeftSecs = dayData.getTimeToWorkLeft();
+                    var timeToWorkLeft = Utils.formatDuration(timeToWorkLeftSecs);
+                    builder.timeToWorkLeft(timeToWorkLeft);
 
-                        var timeToWorkLeftSecs = dayData.getTimeToWorkLeft();
-                        var timeToWorkLeft = Utils.formatDuration(timeToWorkLeftSecs);
-                        builder.timeToWorkLeft(timeToWorkLeft);
+                    var trackedSecs = dayData.getTrackedInSecsProperty().get();
+                    var tracked = Utils.formatDuration(trackedSecs);
+                    builder.tracked(tracked);
 
-                        var trackedSecs = dayData.getTrackedInSecsProperty().get();
-                        var tracked = Utils.formatDuration(trackedSecs);
-                        builder.tracked(tracked);
-
-                        var expectedSecs = dayData.getExpectedTotalInSecsProperty().get();
-                        var expected = Utils.formatDuration(expectedSecs);
-                        builder.expected(expected);
-                    } else {
-                        builder.total("-");
-                    }
-
+                    var expectedSecs = dayData.getExpectedTotalInSecsProperty().get();
+                    var expected = Utils.formatDuration(expectedSecs);
+                    builder.expected(expected);
+                    var date = DATE_FORMAT_WITH_SHORT_DAY_NAME.format(dayData.getDate());
                     return builder.date(date).build();
                 }).collect(collectingAndThen(toList(), FXCollections::observableArrayList));
     }
