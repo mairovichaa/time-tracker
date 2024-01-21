c4335945666c9a1daee7425bc6f04aca5ca73439
andrei.mairovich <mairovichaa@yandex.ru>
2024-01-21 13:03:28 +0400

    Combine total and interval statistics into one column

--- desktop/src/main/java/time_tracker/component/statistics/StatisticsTab.java
+++ desktop/src/main/java/time_tracker/component/statistics/StatisticsTab.java
@@ -4,22 +4,28 @@ import javafx.geometry.Insets;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class StatisticsTab extends Tab {
 
     public StatisticsTab() {
         super("Statistics");
-        var dailyStatisticsVBox = new DailyStatisticsVBox();
-        var weeklyStatisticsVBox = new WeeklyStatisticsVBox();
+
         var intervalStatisticsVBox = new IntervalStatisticsVBox();
         var totalStatisticsVBox = new TotalStatisticsVBox();
+        var firstColumn = new VBox();
+        firstColumn.getChildren().addAll(totalStatisticsVBox, intervalStatisticsVBox);
+        firstColumn.setSpacing(10);
+
+        var weeklyStatisticsVBox = new WeeklyStatisticsVBox();
+        var dailyStatisticsVBox = new DailyStatisticsVBox();
 
         var hBoxWrapper = new HBox();
 
         hBoxWrapper.setSpacing(10);
 
         hBoxWrapper.getChildren()
-                .addAll(totalStatisticsVBox, intervalStatisticsVBox, weeklyStatisticsVBox, dailyStatisticsVBox);
+                .addAll(firstColumn, weeklyStatisticsVBox, dailyStatisticsVBox);
         hBoxWrapper.setPadding(new Insets(10, 0, 0, 10));
 
         var scrollPane = new ScrollPane(hBoxWrapper);
